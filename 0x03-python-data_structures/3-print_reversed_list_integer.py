70bccbeca1e56cfe591093ac3622c855d8e46942
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-01-08 09:06:37 +0000

    Check for None argument given to the function

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,8 @@ def print_reversed_list_integer(my_list=[]):
     """
     print the elements of a given integer list in reverse.
     """
-    for i in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[i]))
+    if my_list:
+        my_list.reverse()
+        for num in my_list:
+            print("{:d}".format(num))
+        my_list.reverse()
