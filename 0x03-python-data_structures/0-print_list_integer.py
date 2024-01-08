572cef88ae3adc3bacdd0eeb481f7f636d330caa
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-01-08 08:55:27 +0000

    Fix docstring to align with conventions

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    """Print the elements of an integer list"""
+    """Print the elements of an integer list."""
     for num in my_list:
         print("{:d}".format(num))
--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     """
-    Return the element at a given index of a given list if possible
+    Return the element at a given index of a given list if possible.
     """
     if idx < 0 or idx >= len(my_list):
         return (None)
