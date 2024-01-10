173aac183548a432dc9205dff13090779bd516e7
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-01-10 16:02:33 +0000

    Fix bug with search and replace function with empty list args and write a function that returns the sum of the integers in a list (once for each integer)

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = None
+    new_list = []
     if my_list:
         new_list = list(map(lambda n: replace if n == search else n, my_list))
     return new_list
