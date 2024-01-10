0e75f8e744f8c81290a6ed55fd875e06c1090688
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-01-10 15:34:47 +0000

    Fix code style and write a function that replaces every instance of a number in a list with another using lambda with map

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
-        new_matrix.append(list(map(lambda num : num ** 2, row)))
+        new_matrix.append(list(map(lambda num: num ** 2, row)))
     return (new_matrix)
