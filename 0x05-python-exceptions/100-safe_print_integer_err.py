4419fd8ea119d943948c6ec8022638df7530de61
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-01-22 20:32:20 +0000

    Fix style to align with PEP8

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 from sys import stderr
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return (True)
-    except ValueError as ve:
+    except (ValueError, TypeError) as ve:
         stderr.write("Exception: {}\n".format(ve))
         return (False)
