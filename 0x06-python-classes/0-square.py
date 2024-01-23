7e0cadf02891b3fa0d42d5fa3460d33c78f1d2d5
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-01-23 19:48:57 +0000

    Init prerequisite files for project and write an empty class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""0-square.py
+
+This script creates an empty class Square
+
+"""
+
+
+class Square(object):
+    """An empty class, the simplest class there is
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,3 @@
+Python classes
+This directory was created on January 23, 2024 as part of the ALX SE Foundations
+program project '0x06. Python - Classes and Objects'.
