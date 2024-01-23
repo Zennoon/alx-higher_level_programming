7c4d0b540549e7c4a93a2db5939f19adaf8576f2
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-01-23 21:53:53 +0000

    Write a class to represent a square with a method to print the square as '#' characters and reflecting its position on the 2d plane

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""6-square.py
+
+Author: Yunus Kedir
+
+This module continues adding further attributes to the Square module from the
+5-square module. This time, it introduces a new private field, along with a
+getter setter pair for it, it also does some modifications on the my_print
+method
+
+"""
+
+
+class Square(object):
+    """Represents a square with some size."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes a newly created instance of the Square class.
+
+        Args:
+            size (int): Size of the square (length of any single side)
+            position (tuple): Position of the square in a 2D axis
+
+        self.size = size
+        self.position = position
+
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Getter setter pair for the private size attribute.
+
+        The setter also does type and value validation for the given size.
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if value.__class__ != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Getter setter pair for the private position attribute.
+
+        The setter also performs type and value validation for the given
+        position.
+        """
+        return (self.__position)
+
+    @position.setter
+    def position(self, tpl):
+        try:
+            if (tpl.__class__ != tuple or tpl[0].__class__ != int
+                    or tpl[0] < 0 or tpl[1].__class__ != int or tpl[1] < 0):
+                raise TypeError("""position must be a tuple of 2 positive
+                        integers""")
+            self.__position = tpl
+        except IndexError:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        """Computes and returns the area of the Square instance.
+
+        Returns:
+            int: The area of the Square instance/object.
+
+        """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """Prints the square as a collection of '#' characters.
+
+        The position of the square is considered to be on a 2D plane, with the
+        1st element of the position tuple being the x offset from the origin,
+        represented by spaces in the printing, and the 2nd element of the tuple
+        signifying the y offset of the square from the origin, represented by
+        blank lines in the printing.
+
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for j in range(self.__position[0]):
+                    print(" ", end="")
+                for k in range(self.__size):
+                    print("#", end="")
+                print()
--- 0x06-python-classes/testsuite.py
+++ 0x06-python-classes/testsuite.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-Square = __import__("5-square").Square
+Square = __import__("6-square").Square
 
-sq = Square(9)
+sq = Square(9, (3, 3))
 print("Area: {} for size: {}".format(sq.area(), sq.size))
 sq.my_print()
 print("--------------------------------------------------------")
@@ -9,10 +9,11 @@ sq.size = 0
 sq.my_print()
 print("--------------------------------------------------------")
 sq.size = 3
+sq.position = (0, 0)
 print("Area: {} for size: {}".format(sq.area(), sq.size))
 sq.my_print()
 try:
-    sq.size = "5 feet"
-    print("Area: {} for size: {}".format(sq.area(), sq.size))
+   sq.size = "5 feet"
+   print("Area: {} for size: {}".format(sq.area(), sq.size))
 except Exception as e:
-    print(e)
+   print(e)
