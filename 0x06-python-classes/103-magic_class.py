5a35b5cf57eabf1e3a0293ed104acf1b3ec6462d
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-01-24 20:27:45 +0000

    Insert docstrings to the class and inner methods

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -8,18 +8,25 @@ This script was created to create python code or a class to be specific, that
 would generate the same bytecode as the one given in the task.
 
 """
-from dis import dis
 
 
 class MagicClass:
+    """The class to implement to imitate the bytecode. Reps a Circle"""
     def __init__(self, radius):
+        """Initializes a newly created MagicClass instance.
+
+        Args:
+            radius: The radius of the circle, can be int or float
+        """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
+        """Calculates and returns the area of an instance."""
         return (self.__radius ** 2) * math.pi
 
     def circumference(self):
+        """Calculates and returns the circumference of an instance."""
         return (2 * math.pi * self.__radius)
