8677e364cad5fd267a8a327bfea89b0baa5c965b
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-01-30 19:49:26 +0000

    Write a class which contains a list, __slots__, that holds the only possible attributes of instances of the class

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""101-locked_class.py
+
+Author: Yunus Kedir
+
+This module contains an empty class that has a __slots__ list preventing the
+creation of any dynamically created instance attributes (created after the
+instance creation) except those whose name are in the list.
+"""
+
+
+class LockedClass(object):
+    """An empty class with a __slots__ list listing the only possible attributes
+    to create for an instance of the class."""
+    __slots__ = ["first_name"]
