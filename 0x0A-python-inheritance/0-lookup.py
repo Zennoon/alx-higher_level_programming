b256e6af4f5035710d6c327b119bd18bf96a179a
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-02-05 17:40:44 +0000

    Init prerequisite files and write a function that returns the available attributes of a given object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""0-lookup.py
+
+Author: Yunus Kedir
+
+This module contains a function that can be used to get a list of the available
+attributes (fields + methods) that an object has available.
+
+"""
+
+
+def lookup(obj):
+    """Returns a list of all available attributes of a given object.
+
+    Args:
+        obj: An arbitrary object.
+
+    Returns:
+        list<str>: A list of the attributes of obj (empty list if obj == None).
+    """
+    attrs = []
+    if obj is not None:
+        attrs = list(obj.__dict__.keys())
+    return (attrs)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,2 @@
+This directory was created on February 5, 2024 as part of the ALX SE Foundations
+project '0x0A. Python - Inheritance'.
