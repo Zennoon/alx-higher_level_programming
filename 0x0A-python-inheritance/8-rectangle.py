8ab41cc7d725ab8a915db167b9d227a96d013053
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-02-05 20:40:53 +0000

    Write a class that inherits from the BaseGeometry class and uses its integer_validator function during instantiation

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""8-rectangle.py
+
+Author: Yunus Kedir
+
+This module contains:
+
+Classes:
+    BaseGeometry: base class for other classes, has 2 instance methods for now.
+    Rectangle: Inherits from BaseGeometry. Represents a rectangle.
+
+"""
+
+
+class BaseGeometry(object):
+    """A base class for future tasks with subclasses."""
+    def area(self):
+        """Raises an Exception that it has not been implemented yet."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates the type and value of the given value.
+
+        Args:
+            name (str): Name of the value
+            value (int): The value to validate
+        """
+        if value.__class__ is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle. Inherits from the BaseGeometry class."""
+    def __init__(self, width, height):
+        """Initiates a newly created Rectangle instance.
+
+        Args:
+            width (int): Width of the rectangle.
+            height (int):height of the rectangle.
+
+        Both the width and height values are validated using the
+        integer_validator function found in the base class.
+        """
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
