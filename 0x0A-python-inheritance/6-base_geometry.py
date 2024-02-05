6c81112b3a7f3f7cc7eef6207f74299f4bd93d24
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-02-05 19:45:12 +0000

    Write a class with an instance method that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""6-base_geometry.py
+
+Author: Yunus Kedir
+
+This module contains:
+
+Classes:
+    BaseGeometry: Has an unfinished area method, raises an Exception for now.
+
+"""
+
+
+class BaseGeometry(object):
+    """A base class for future tasks with subclasses."""
+    def area(self):
+        """Raises an Exception that it has not been implemented yet."""
+        raise Exception("area() is not implemented")
