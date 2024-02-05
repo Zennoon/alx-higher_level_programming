c7a180ff539bd544fa7691da2499a58864cfc363
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-02-05 19:39:18 +0000

    Write an empty class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""5-base_geometry.py
+
+Author: Yunus Kedir
+
+This module contains:
+
+Classes:
+    BaseGeometry: An empty class that will serve as a base class for future
+                  tasks.
+
+"""
+
+
+class BaseGeometry(object):
+    """An empty class."""
+    pass
