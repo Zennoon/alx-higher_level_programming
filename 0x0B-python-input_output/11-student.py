8ba2f3611f76fbe3403895d410c2c0455e65d774
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-02-06 21:10:24 +0000

    Write a class with a public instance method that replaces the attributes of the instance using another dictionary

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""10-student.py
+
+Author: Yunus Kedir
+
+This module contains:
+
+Classes:
+    Student - Ordinary class with a method that returns an instance's
+              dictionary representation of selected attributes, and a
+              method for replacing the attributes of an instance using
+              another dictionary.
+
+"""
+
+
+class Student(object):
+    """Simple class that represents a student with basic attributes."""
+    def __init__(self, first_name, last_name, age):
+        """Initiates a newly created Student instance.
+
+        Args:
+            first_name (str): First name of the student instance.
+            last_name (str): Last name of the student instance.
+            Age (int): Age of the student instance.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns a dictionary representation of the calling instance that
+        only includes the attribute names found in a given list if it is valid.
+
+        Args:
+            attrs (list<str>): A list of attribute names to include in the
+            returned dictionary.
+
+        Returns:
+            dict: A dictionary of the attributes of the instance that are in
+                  attrs if it is a valid list of strings.
+        """
+        if attrs is not None:
+            dict_repr = {}
+            for attr in attrs:
+                if self.__dict__.get(attr):
+                    dict_repr[attr] = self.__dict__.get(attr)
+            return (dict_repr)
+        return (self.__dict__)
+
+    def reload_from_json(self, json):
+        """Replaces the attributes of calling instances with the values in
+        a given dictionary.
+
+        Args:
+            json: The dictionary to replace from.
+        """
+        self.__dict__ = json
