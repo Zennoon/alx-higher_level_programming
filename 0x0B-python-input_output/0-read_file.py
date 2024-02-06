be71c63f4dca0140a489aba29f3bcd1a564410fe
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-02-06 18:32:09 +0000

    Init prerequisite files and write a function that reads and prints the contents of a file of given name

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""0-read_file.py
+
+Author: Yunus Kedir
+
+This module contains:
+
+Functions:
+    read_file - Reads the content of a file of given name.
+
+"""
+
+
+def read_file(filename=""):
+    """Prints out the contents of a text file of given name to stdout.
+
+    Args:
+        filename (str): Name of the file to open and read.
+    """
+    with open(filename, mode='r', encoding="utf-8") as f:
+        print(f.read())
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,2 @@
+This directory was created on February 6, 2024 as part of the ALX SE Foundations
+project '0x0B. Python - Input/Output'.
