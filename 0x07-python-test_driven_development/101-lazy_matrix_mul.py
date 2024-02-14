2a5455c6dce63da8d23a4ed735fe070e82ab7248
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-02-14 09:43:50 +0000

    Write a function that multiplies matrices using the numpy library

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8
+""" 101-lazy_matrix_mul
+
+This module contains:
+
+Functions:
+    lazy_matrix_mul - Multiplies two matrices using numpy and returns result as
+                      list
+
+"""
+
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    if m_a == [] or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+
+    if m_b == [] or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
+
+    return ([list(arr) for arr in np.matmul(m_a, m_b)])
