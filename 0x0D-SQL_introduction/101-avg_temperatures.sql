2ef90ee309d998ac836eefc740c57c9d565c3540
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-03-12 20:57:09 +0000

    Fix typo

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- Displays average temperature by City of the temperature table in hbtn_0c_0
-SELECT city, AVG(value) AS avg_tmp FROM temperatures GROUP BY city ORDER BY AVG(value) DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY AVG(value) DESC;
