c64a1fd3e76f08384353048b062d1390c61b5fcb
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-03-12 19:07:59 +0000

    Modify the query to output the records sorted by descending values of score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,3 @@
 -- Displays the number of records for every distinct score in a table.
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(score) AS number FROM second_table
+GROUP BY score ORDER BY score DESC;
