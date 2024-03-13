a4d61a6813334a5801b47b5d37dc2cdc1699df30
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-03-13 22:13:39 +0000

    Write an SQL script that uses MINUS/EXCEPT to display the difference between two queries

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,30 @@
+-- Displays all shows without the genre Comedy.
+SELECT tv_shows.title AS title
+  FROM tv_shows
+
+EXCEPT
+
+SELECT tv_shows.title AS title
+  FROM tv_genres
+       INNER JOIN tv_show_genres
+       ON tv_genres.id = tv_show_genres.genre_id
+       AND tv_genres.name = 'Comedy'
+
+       INNER JOIN tv_shows
+       ON tv_show_genres.show_id = tv_shows.id
+ ORDER BY title;
+
+/** Alternatively, we can utilize this query which uses a subquery
+ * SELECT tv_shows.title AS title
+ *   FROM tv_shows
+ *  WHERE tv_shows.id NOT IN
+ *        (SELECT tv_shows.id
+ *         FROM tv_genres
+ *      	      INNER JOIN tv_show_genres
+ *      	      ON tv_genres.id = tv_show_genres.genre_id
+ *      	      AND tv_genres.name = 'Comedy'
+ *
+ *       	      INNER JOIN tv_shows
+ *      	      ON tv_show_genres.show_id = tv_shows.id)
+ *  ORDER BY title;
+ */
