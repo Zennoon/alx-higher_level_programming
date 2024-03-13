1ed3a06c3828b7151008b988b3b8bdcd91b1a27a
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-03-13 20:53:38 +0000

    Write an SQL script that extracts values from left table for which there is no matching value in the right table

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- Displays all tv shows in a database regardless of whether they are linked
+-- to a genre or not.
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+  FROM tv_shows
+       LEFT JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
+       WHERE tv_show_genres.show_id IS NULL
+ ORDER BY title, genre_id
