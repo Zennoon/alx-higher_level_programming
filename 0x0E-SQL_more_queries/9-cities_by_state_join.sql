920b53472efc7e5230457f008086064ad3211c38
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-03-13 20:24:39 +0000

    Write an SQL script that joins two tables to extract desired data

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- Displays all the cities in the database along with their states' name.
+SELECT cities.id, cities.name, states.name
+  FROM cities
+       INNER JOIN states
+       ON cities.state_id = states.id
+ ORDER BY cities.id;
