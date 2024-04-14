b4c091445e15de0627d7f97054fe7c49bbf93fcc
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-04-14 16:54:45 +0000

    Write a script that retrieves records of a table whose name field contains 'a'

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+
+Author: Yunus Kedir
+
+Contains:
+    script that retrieves the records of a table that contiain 'a' from a given
+    database
+"""
+import sqlalchemy
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    conn = "mysql+mysqldb://{}:{}@localhost:3306/{}"
+    engine = create_engine(conn.format(args[1], args[2], args[3]))
+    session = sessionmaker(bind=engine)()
+    states = session.query(State).filter(State.name.like("%a%"))
+    states = states.order_by(State.id).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
