f94849e776606d5c52ec34378c38a946c82db05a
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-04-14 15:27:35 +0000

    Write a python script that connects to a database table via sqlalchemy and retrieves all records of a table

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+
+Author: Yunus Kedir
+
+Contains:
+    script that connects to a database using SQLAlchemy and retrieves all
+    records of a table 'states'
+"""
+import sqlalchemy
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+args = sys.argv
+conn = "mysql+mysqldb://{}:{}@localhost:3306/{}"
+engine = create_engine(conn.format(args[1], args[2], args[3]))
+session = sessionmaker(bind=engine)()
+states = session.query(State).order_by(State.id).all()
+for idx, state in enumerate(states):
+    print("{}: {}".format(idx + 1, state.name))
