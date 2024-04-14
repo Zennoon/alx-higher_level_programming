bad9dcdd0493bd08c2b201924a372436ffb14d4b
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-04-14 15:32:53 +0000

    Dispose of pool of connections after use

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -19,6 +19,7 @@ args = sys.argv
 conn = "mysql+mysqldb://{}:{}@localhost:3306/{}"
 engine = create_engine(conn.format(args[1], args[2], args[3]))
 session = sessionmaker(bind=engine)()
-states = session.query(State).order_by(State.id).all()
+states = session.query(State.name).order_by(State.id).all()
 for idx, state in enumerate(states):
     print("{}: {}".format(idx + 1, state.name))
+engine.dispose()
