dadef1d0bbf5f07cc1918038d79e6347c0598aca
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-04-14 22:10:29 +0000

    Write a python script that retrieves state records and prints them along with their cities

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+
+Author: Yunus Kedir
+
+Contains:
+    Script that uses the relationship established between classes
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    conn = "mysql+mysqldb://{}:{}@localhost:3306/{}"
+    engine = create_engine(conn.format(args[1], args[2], args[3]))
+    session = sessionmaker(bind=engine)()
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
+    engine.dispose()
