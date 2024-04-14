c4c5f3df55302223c8cbf77465846cc49cb4771d
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-04-14 13:16:31 +0000

    Write a python script that accesses a database and lists all records with a field value equal to a string given as a command line argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+
+Author: Yunus Kedir
+
+Contains:
+    script that accesses a mysql database using MySQLdb and lists all states
+    matching a given name from states table. The user, password, database to
+    access, and state name to search for are given as command line arguments
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    conn = MySQLdb.connect(user=args[1], passwd=args[2], db=args[3])
+    cursor = conn.cursor()
+    q = "SELECT * FROM states WHERE states.name = '{}' ORDER BY states.id"
+    cursor.execute(q.format(args[4]))
+    items = cursor.fetchall()
+    for item in items:
+        if item[1][0].isupper():
+            print(item)
+    cursor.close()
+    conn.close()
