6bbbfb461dda38363934f98e58cfe45d630723b2
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-04-25 18:03:22 +0000

    Make script executable

--- 0x10-python_network_0/0-body_size.sh
+++ 0x10-python_network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Curls a given url and displays size of the body of the response
+# Curls a given url and displays size of the body of the response (Content)
 curl -sI "$1" | grep -E "Content-Length: .*" | cut -d ":" -f2 | tr -d " "
