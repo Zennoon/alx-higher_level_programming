fe9ea4e3b0bc794b7d6facd8d3f58293a345cc26
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-05-21 17:45:26 +0000

    Fix semistandard issues

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,13 +3,12 @@
 const args = process.argv;
 const filepath = args[2];
 
-const fs = require("fs");
+const fs = require('fs');
 
-fs.readFile(filepath, "utf8", (err, data) => {
-    if (err) {
-	console.log(err);
-    }
-    else {
-	console.log(data);
-    }
+fs.readFile(filepath, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
