f3e7bc2ba662605457ee6c58866afd550ff52650
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-05-21 17:42:37 +0000

    Write a JS script that reads contents of a given file and displays it

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const filepath = args[2];
+
+const fs = require("fs");
+
+fs.readFile(filepath, "utf8", (err, data) => {
+    if (err) {
+	console.log(err);
+    }
+    else {
+	console.log(data);
+    }
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,2 @@
+This directory was created on May 21, 2024 as part of the ALX SE Foundations
+project '0x14. Javascript - Web scraping'.
